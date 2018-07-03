d876f5bd011acbc579f219e442fd2c905e85294b
andrewt-gh <39068563+andrewt-gh@users.noreply.github.com>
2018-07-03 11:11:03 -0500

    Create objectExample.js

---
+++ JavaScript/Objects/objectExample.js
@@ -0,0 +1,20 @@
+let person = {
+  name: 'Andrew',
+  age: 26
+};
+
+// Write a console.log() under your person object. Inside of it, use dot notation to print your name to the console.
+
+console.log(person.name);
+
+// Create another console.log() to log your age key from within the person variable.
+
+console.log(person.age);
+
+// Modify the dot notation selector for the name key you wrote in the last exercise to use bracket notation instead.
+
+console.log(person['name']);
+
+// Now, modify the age key to use bracket notation instead of dot notation as well.
+
+console.log(person['age']);
