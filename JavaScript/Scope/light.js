b4813fdb329b58e1e0c7f5c6e4b72875ce21780a
andrewt-gh <39068563+andrewt-gh@users.noreply.github.com>
2018-07-03 11:12:43 -0500

    Create light.js

---
+++ JavaScript/Scope/light.js
@@ -0,0 +1,8 @@
+// Write a function with const
+const visibleLightWaves = () => {
+// Use let to create a variable
+let lightWaves = 'Moonlight';
+console.log(lightWaves);
+}
+// Call the function visibleLightWaves
+visibleLightWaves(); // Output: Moonlight
