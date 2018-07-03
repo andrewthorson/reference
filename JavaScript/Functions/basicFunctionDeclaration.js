d9a34438145b0ce7ac52f1f36d4b1b9ac16617f6
andrewt-gh <39068563+andrewt-gh@users.noreply.github.com>
2018-07-03 11:02:20 -0500

    Create basicFunctionDeclaration.js

---
+++ JavaScript/Functions/basicFunctionDeclaration.js
@@ -0,0 +1,13 @@
+// Create new function called isGreaterThan
+function isGreaterThan (numberOne, numberTwo) {
+// Create if/else statement to check if numberOne is greater than numberTwo
+if (numberOne > numberTwo) {
+	return true;
+} else {
+	return false;
+}
+}
+// Call isGreaterThan() function with two arguments
+console.log(isGreaterThan(2,3)); // Output: false
+// Call isGreaterThan() function with two arguments
+console.log(isGreaterThan(3,2)); // Output: true
