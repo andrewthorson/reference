9c5db407fff32a5296fcae0c5835bd7b7b9e786f
andrewt-gh <39068563+andrewt-gh@users.noreply.github.com>
2018-07-03 11:02:54 -0500

    Create pizzaRestaurantOrders.js

---
+++ JavaScript/Functions/pizzaRestaurantOrders.js
@@ -0,0 +1,7 @@
+// Create takeOrder variable and set to function ()=> with parameter topping
+const takeOrder = (topping) => {
+// Print string with argument passed to parameter
+	console.log('Order: pizza topped with ${topping}');
+}
+// Run function
+takeOrder('pepperoni');
