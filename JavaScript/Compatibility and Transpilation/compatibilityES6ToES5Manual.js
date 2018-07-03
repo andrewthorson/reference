bf919e8d4e0ec1d7eb43208cc831f5c296385bf6
andrewt-gh <39068563+andrewt-gh@users.noreply.github.com>
2018-07-03 11:00:18 -0500

    Create compatibilityES6ToES5Manual.js

---
+++ JavaScript/Compatibility and Transpilation/compatibilityES6ToES5Manual.js
@@ -0,0 +1,25 @@
+var pasta = "Spaghetti"; // ES5 syntax
+
+const meat = "Pancetta"; // ES6 syntax
+
+let sauce = "Eggs and cheese"; // ES6 syntax
+
+// Template literals, like the one below, were introduced in ES6
+const carbonara = `You can make carbonara with ${pasta}, ${meat}, and a sauce made with ${sauce}.`;
+
+/*
+In these tasks, you will manually change the code in main.js to ES5 syntax.
+
+The let and const keywords were introduced in ES6. Before that, we declared all variables with the var keyword.
+
+Change the let and const keywords in main.js to var.
+*/
+
+var pasta = "Spaghetti"; // ES5 syntax
+
+var meat = "Pancetta"; // ES6 syntax
+
+var sauce = "Eggs and cheese"; // ES6 syntax
+
+// Template literals, like the one below, were introduced in ES6
+var carbonara = 'You can make carbonara with ' + pasta + ', ' + meat + ", and a sauce made with " + sauce + ".";
