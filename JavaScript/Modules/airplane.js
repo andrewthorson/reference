2bf5e2e79eb3b96e0e5548f88ac88e4204ab44f2
andrewt-gh <39068563+andrewt-gh@users.noreply.github.com>
2018-07-03 11:14:56 -0500

    Create airplane.js

---
+++ JavaScript/Modules/airplane.js
@@ -0,0 +1,49 @@
+// Default Export
+let Airplane = {
+  availableAirplanes: [
+    {name: 'AeroJet',
+    fuelCapacity: 800},
+    {name: 'SkyJet',
+    fuelCapacity: 500}
+  ],
+};
+export default Airplane;
+
+//Named Export
+let availableAirplanes = [{
+ name: 'AeroJet',
+ fuelCapacity: 800,
+ availableStaff: ['pilots', 'flightAttendants', 'engineers', 'medicalAssistance', 'sensorOperators'],
+ maxSpeed: 1200,
+ minSpeed: 300
+}, 
+{name: 'SkyJet',
+ fuelCapacity: 500,
+ availableStaff: ['pilots', 'flightAttendants'],
+ maxSpeed: 800,
+ minSpeed: 200
+}];
+
+let flightRequirements = {
+  requiredStaff: 4,
+  requiredSpeedRange: 700
+};
+
+function meetsStaffRequirements(availableStaff, requiredStaff) {
+  if (availableStaff.length >= requiredStaff) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+function meetsSpeedRangeRequirements(maxSpeed,minSpeed,requiredSpeedRange) {
+  let range = maxSpeed - minSpeed;
+  if (range > requiredSpeedRange) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export {availableAirplanes as aircrafts, flightRequirements as flightReqs, meetsStaffRequirements as meetsStaffReqs, meetsSpeedRangeRequirements as meetsSpeedRangeReqs};
