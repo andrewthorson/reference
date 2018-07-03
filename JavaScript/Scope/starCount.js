7f2a98a3304f5b601e15367c5f1bb7c1506bb102
andrewt-gh <39068563+andrewt-gh@users.noreply.github.com>
2018-07-03 11:13:08 -0500

    Create starCount.js

---
+++ JavaScript/Scope/starCount.js
@@ -0,0 +1,13 @@
+// Using const, declare a new function called starCount().
+const starCount = () => {
+// Within the starCount() function, declare a let variable named i and set it equal to 5.
+let i = 5;
+// Right beneath the variable declaration, log the value of the i value to the console.
+console.log(i);
+// Beneath the previous console.log() statement, create a for loop.
+for (let i = 0; i < 12; i++) {
+	console.log(i);
+}
+}
+// Call starCount() function, from outside of the function.
+starCount();
