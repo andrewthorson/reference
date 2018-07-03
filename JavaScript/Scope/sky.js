073b5ead70d6c2308ab29b5a44dc61b864fe5e5d
andrewt-gh <39068563+andrewt-gh@users.noreply.github.com>
2018-07-03 11:12:56 -0500

    Create sky.js

---
+++ JavaScript/Scope/sky.js
@@ -0,0 +1,9 @@
+// Write two global const variables
+const satellite = 'The Moon'
+const galaxy = 'The Milky Way'
+// Write global variable using let
+let stars = 'North Star'
+// Using const create a function named myNightSky
+const myNightSky = () => 'Night Sky: ' + satellite + ', ' + stars + ', ' + galaxy;
+// Call myNightSky and log to console
+console.log(myNightSky()); // Output: Night Sky: The Moon, North Star, The Milky Way
