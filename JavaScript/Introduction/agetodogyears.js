aca6eb2e15bbe87899c968f0605d0f6586468c26
andrewt-gh <39068563+andrewt-gh@users.noreply.github.com>
2018-07-03 11:05:00 -0500

    Create agetodogyears.js

---
+++ JavaScript/Introduction/agetodogyears.js
@@ -0,0 +1,16 @@
+// Create myAge and set to age value
+let myAge = 26;
+// Create earlyYears and set value of 2
+let earlyYears = 2;
+// Use multiplication assignment operator to multiply earlyYears by 10.5
+earlyYears *= 10.5;
+// Subtract two years from myAge and assign as new variable
+let laterYears = myAge - 2;
+// Multiply laterYears by 4 to calculate the number of dog years accounted for by your later years.
+laterYears *= 4;
+// Create new variable myAgeInDogYears by adding together earlyYears and laterYears
+let myAgeInDogYears = earlyYears + laterYears;
+// Save name to new variable as string in lower case
+let myName = 'Andrew Thorson'.toLowerCase();
+// Print name and age in dog years
+console.log(`My name is ${myName}. I am ${myAgeInDogYears}.`)
