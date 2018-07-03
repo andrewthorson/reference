1ef6a5afff3064ce91bc2f6e8ec576d2aec5c1c1
andrewt-gh <39068563+andrewt-gh@users.noreply.github.com>
2018-07-03 11:09:12 -0500

    Create loopingManually.js

---
+++ JavaScript/Loops/loopingManually.js
@@ -0,0 +1,7 @@
+// Write an array and set it equal to a variable named vacationSpots.
+// Inside of this array, list three places you'd like to visit.
+let vacationSpots = ['Detroit Lakes', 'Minneapolis', 'Fargo']
+// Next, console.log() each item in vacationSpots on a separate line. To do this, list out each item using property access.
+console.log(vacationSpots[0]);
+console.log(vacationSpots[1]);
+console.log(vacationSpots[2]);
