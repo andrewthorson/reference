bc150b895cf4debc5d41f5164d9b7a5ec3d3b447
Andrew Thorson <39068563+andrewthorson@users.noreply.github.com>
2022-05-13 17:12:36 -0400

    Create Tree.java

---
+++ Java/Tree.java
@@ -0,0 +1,35 @@
+/**
+* <h1>Planting a Tree</h1>
+* This class introduces the author and plants a tree
+* for the author.
+*
+* @author  Andrew Thorson
+* @version 1.0
+* @since   2022-05-12
+*/
+public class Tree {
+  public static void main(String[] args) {
+  /*
+    This method introduces the author and prints a screen
+    to the console.
+  */
+
+  // Print a message introducing the author.
+  System.out.println("Hey there, I'm Andrew and I'm learning to code in Java!");
+
+  // Print a message indicating the goal of the project.
+  System.out.println("I'm going to plant a tree today!");
+
+  // Use multiple print statements to plant a tree.
+  System.out.println("");
+  System.out.println("   *********   ");
+  System.out.println("  * * *** * *  ");
+  System.out.println(" * *  ***  * * ");
+  System.out.println("      ***      ");
+  System.out.println("      ***      ");
+  System.out.println("      ***      ");
+  System.out.println("      ***      ");
+  System.out.println("^~^~^~^~^~^~^~^");
+
+  }
+}
